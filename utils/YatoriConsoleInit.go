339b26c234d0124535840eacb1f9681d7c9cf135
Changbaiqi <changbaiqi@foxmail.com>
2025-04-28 18:11:45 +0800

    update: 优化日志显示，现在Windows10无意外可支持彩色日志显示。

--- utils/YatoriConsoleInit.go
+++ utils/YatoriConsoleInit.go
@@ -2,8 +2,11 @@ package utils
 
 import (
 	_ "embed"
+	lg "github.com/yatori-dev/yatori-go-core/utils/log"
+	"golang.org/x/sys/windows/registry"
 	"log"
 	"os"
+	"runtime"
 
 	"github.com/yatori-dev/yatori-go-core/utils"
 )
@@ -19,6 +22,7 @@ func YatoriConsoleInit() {
 	if !f1 {
 		writeDLLToDisk() //确保文件都加载了
 	}
+	initConsole()
 }
 
 // 将必要文件复制到当前目录下
@@ -28,5 +32,25 @@ func writeDLLToDisk() {
 	f1 := os.WriteFile(noticePath, noticeSound, 0644)
 	if f1 != nil {
 		log.Fatal(f1)
+	}
+}
+
+// 初始化控制台配置
+func initConsole() {
+	sysType := runtime.GOOS
+	if sysType == "windows" {
+		//修改
+		// 打开（或创建）指定的注册表键
+		key, _, err := registry.CreateKey(registry.CURRENT_USER, `Console`, registry.SET_VALUE)
+		if err != nil {
+			lg.Print(lg.INFO, lg.Red, "打开注册表 Console 失败", err.Error())
+		}
+		defer key.Close()
+
+		// 设置 VirtualTerminalLevel 的值为 1 (DWORD类型)
+		err = key.SetDWordValue("VirtualTerminalLevel", 1)
+		if err != nil {
+			lg.Print(lg.INFO, lg.Red, "设置注册表 Console/VirtualTerminalLevel = 1 失败", err.Error())
+		}
 	}
 }
