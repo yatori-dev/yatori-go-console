21f6d622e36ae3cd61931bc22abf3eda1c76caba
Changbaiqi <changbaiqi@foxmail.com>
2025-05-23 22:02:51 +0800

    update: 优化学时提交

--- logic/xuexitong/XueXiTongPart.go
+++ logic/xuexitong/XueXiTongPart.go
@@ -296,7 +296,7 @@ func ExecuteVideo2(cache *xuexitongApi.XueXiTUserCache, p *entity.PointVideoDto)
 			}
 			lg.Print(lg.INFO, "[", lg.Green, cache.Name, lg.Default, "] ", " 【", p.Title, "】 >>> ", "提交状态：", lg.Green, lg.Green, strconv.FormatBool(gojsonq.New().JSONString(playReport).Find("isPassed").(bool)), lg.Default, " ", "观看时间：", strconv.Itoa(playingTime)+"/"+strconv.Itoa(p.Duration), " ", "观看进度：", fmt.Sprintf("%.2f", float32(playingTime)/float32(p.Duration)*100), "%")
 
-			if overTime >= 60 { //过超提交触发
+			if overTime >= 200 { //过超提交触发
 				lg.Print(lg.INFO, lg.INFO, "[", lg.Green, cache.Name, lg.Default, "] ", " 【", p.Title, "】 >>> ", "过超提交中。。。。")
 				break
 			}
@@ -308,8 +308,8 @@ func ExecuteVideo2(cache *xuexitongApi.XueXiTUserCache, p *entity.PointVideoDto)
 				overTime += 2
 				time.Sleep(2 * time.Second)
 			} else { //正常计时逻辑
-				playingTime = playingTime + 58
-				time.Sleep(58 * time.Second)
+				playingTime = playingTime + 60
+				time.Sleep(60 * time.Second)
 			}
 		}
 	} else {
